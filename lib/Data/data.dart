14498cf1adc04200a28a12ced07f31f509a8c4b7
manoj.waghmare@apptware.com <manoj.waghmare@apptware.com>
2021-09-25 17:48:53 +0530

    - design profile screen with data

---
+++ lib/Data/data.dart
@@ -0,0 +1,49 @@
+import '../models/models.dart';
+
+List<Shows> showsList = [
+  Shows(
+    showName: "Teen Wolf",
+    showStatus: "Ended",
+    showNetworks: "MTV",
+    showPosterUrl: "assets/images/shows/show_0.jpg",
+  ),
+  Shows(
+    showName: "Flash",
+    showStatus: "Continuing",
+    showNetworks: "The CW",
+    showPosterUrl: "assets/images/shows/show_1.jpg",
+  ),
+  Shows(
+    showName: "Arrow",
+    showStatus: "Ended",
+    showNetworks: "The CW",
+    showPosterUrl: "assets/images/shows/show_2.jpg",
+  ),
+  Shows(
+    showName: "Money Heist",
+    showStatus: "Continuing",
+    showNetworks: "Netflix",
+    showPosterUrl: "assets/images/shows/show_3.jpg",
+  ),
+  Shows(
+    showName: "The Walking Dead",
+    showStatus: "Continuing",
+    showNetworks: "AMC",
+    showPosterUrl: "assets/images/shows/show_4.jpg",
+  ),
+  Shows(
+    showName: "Lucifer",
+    showStatus: "Ended",
+    showNetworks: "Netflix",
+    showPosterUrl: "assets/images/shows/show_5.jpg",
+  ),
+];
+
+List<Movies> moviesList = [
+  Movies(movieName: "Kung Fu Panda", movieGenre: "Animation / Comedy", movieYear: "2008"),
+  Movies(movieName: "Thor: The Dark World", movieGenre: "Action / Fantasy", movieYear: "2013"),
+  Movies(movieName: "Abominable", movieGenre: "Animation / Comedy", movieYear: "2019"),
+  Movies(movieName: "Suicide Squad", movieGenre: "Sci-Fi / Action", movieYear: "2016"),
+  Movies(movieName: "Black Panther", movieGenre: "Sci-Fi / Adventure", movieYear: "2018"),
+  Movies(movieName: "The Conjuring", movieGenre: "Horror / Mystery", movieYear: "2013"),
+];
---
+++ lib/models/models.dart
@@ -0,0 +1,27 @@
+class Shows {
+  String showName;
+  String showStatus;
+  String showNetworks;
+  String showPosterUrl;
+
+  Shows({
+    this.showName,
+    this.showStatus,
+    this.showNetworks,
+    this.showPosterUrl,
+  });
+}
+
+class Movies {
+  String movieName;
+  String movieYear;
+  String movieGenre;
+  String moviePosterUrl;
+
+  Movies({
+    this.movieName,
+    this.movieYear,
+    this.movieGenre,
+    this.moviePosterUrl,
+  });
+}
