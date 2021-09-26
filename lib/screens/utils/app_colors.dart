03ba23e98ceed857657a56c210c18d3b72378390
manoj.waghmare@apptware.com <manoj.waghmare@apptware.com>
2021-09-26 15:23:22 +0530

    - design discover screen with data

--- lib/screens/utils/app_colors.dart
+++ lib/screens/utils/app_colors.dart
@@ -2,8 +2,11 @@ import 'package:flutter/material.dart';
 
 class AppColors {
   AppColors(this.primaryColor);
+
   final Color primaryColor;
-  static const Color yellowColor = Colors.yellowAccent;
+
+  // static const Color yellowColor = Colors.yellow;
+  static const Color yellowColor = Color(0xffFFEF35);
   static const Color blackColor = Color(0xFF000000);
   static const Color greyColor = Colors.grey;
   static const Color whiteColor = Colors.white;
